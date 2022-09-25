0d8a1098c939a2a1af2856893ffffe397a310659
gayathra <ngayathra@gmail.com>
2022-09-25 21:11:28 +0530

    added few code lines and commented some

--- Begining/main.py
+++ Begining/main.py
@@ -1,1 +1,7 @@
-print ("hello world")
+#print ("Hello" + str(56) + "um here")
+#print ( f"01 age is around {28} now" )
+
+days = 24
+calculation_to_units = "hours"
+
+#print (f"hello {days} {calculation_to_units}")
