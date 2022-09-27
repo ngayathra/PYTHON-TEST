cb9355da029461fc6d390dc98e08204c3b7b417a
gayathra <ngayathra@gmail.com>
2022-09-27 05:40:55 +0530

    variable scope and functions

--- Begining/main.py
+++ Begining/main.py
@@ -1,10 +1,27 @@
-#print ("Hello" + str(56) + "um here")
-#print ( f"01 age is around {28} now" )
+calculation_to_units = 24
+name_of_units = "hours"
 
-days = 24
-calculation_to_units = "hours"
-#print (f"hello {days} {calculation_to_units}")
-def myfunction():
-    print ("hello this is from the function")
-    print ("my new line")
-myfunction()
+
+"""
+print (f"20 days are {20 * calculation_to_units} {name_of_units}")
+print (f"35 days are {35 * calculation_to_units} {name_of_units}")
+print (f"50 days are {50 * calculation_to_units} {name_of_units}")
+print (f"110 days are {110 * calculation_to_units} {name_of_units}")
+
+"""
+
+def myfuncion(num_of_days, custom_message):
+    print (f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_units}")
+    print (custom_message)
+
+def scope_check():
+    another_value = "1"
+    num_of_days = 2
+    print (calculation_to_units)
+    print (another_value)
+    print (num_of_days)
+
+
+myfuncion(15, "hello")
+myfuncion(20, "wow")
+scope_check()
