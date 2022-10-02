1a4164a2dacb63cf691f99e64b4a039758ec65a7
gayathra <ngayathra@gmail.com>
2022-10-02 20:01:19 +0530

    loops chacked

--- Begining/list.py
+++ Begining/list.py
@@ -1,8 +1,27 @@
 my_list = ["jan", "feb", "march", "april"]
+second_list = ["rice", "nasi", "curry"]
 
 my_list.append("dec")
 
 # print (my_list[4])
 new_list = ""
-for x in my_list:
-    print(x)
+# for x in my_list:
+    # print(x)
+# my_list.append("MAY")
+# my_list.insert(2, "gayatra")
+# my_list.extend(second_list)
+
+# print (my_list)
+
+
+# [print(x) for x in my_list]
+# for x in my_list:
+#     print (x)
+
+# for i in range(len(my_list)):
+#     print (my_list[i])
+
+x = 0
+while x < len(my_list):
+    print (my_list[x])
+    x = x + 1
