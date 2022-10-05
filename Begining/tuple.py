ee5675b01c3e87a820818eaeeb7650c4df5bd2c1
gayathra <ngayathra@gmail.com>
2022-10-05 09:00:11 +0530

    commented

--- Begining/tuple.py
+++ Begining/tuple.py
@@ -1,18 +1,18 @@
 first_tuple = ("apple",)
-print (first_tuple)
-print (type(first_tuple))
+# print (first_tuple)
+# print (type(first_tuple))
 
 second_tuple = ("apple", "banana", "cherry", "apple", "cherry")
 
 #checking the tuple length
-print (second_tuple)
-print (len(second_tuple))
-print ("-------------------------------------")
+# print (second_tuple)
+# print (len(second_tuple))
+# print ("-------------------------------------")
 
 #creating a tuple with it's constructor
 constructor_tuple = tuple(("apple", "banana", "cherry", "apple", "cherry"))
 
-print (constructor_tuple)
+# print (constructor_tuple)
 
 
 #checking an item in a tuple
@@ -21,3 +21,8 @@ if "chery" in second_tuple:
 else:
     print ("no we did't get it")
 
+
+#copying a tuple to a list
+tuplelist = list(second_tuple)
+# print (type(tuplelist))
+# print (tuplelist)
