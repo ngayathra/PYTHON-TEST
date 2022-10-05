38044c7c2900cbbf3aa48c9421656e304a1680cb
gayathra <ngayathra@gmail.com>
2022-10-05 05:48:54 +0530

    checked the basics of tuple

---
+++ Begining/tuple.py
@@ -0,0 +1,23 @@
+first_tuple = ("apple",)
+print (first_tuple)
+print (type(first_tuple))
+
+second_tuple = ("apple", "banana", "cherry", "apple", "cherry")
+
+#checking the tuple length
+print (second_tuple)
+print (len(second_tuple))
+print ("-------------------------------------")
+
+#creating a tuple with it's constructor
+constructor_tuple = tuple(("apple", "banana", "cherry", "apple", "cherry"))
+
+print (constructor_tuple)
+
+
+#checking an item in a tuple
+if "chery" in second_tuple:
+    print ("yes we got it")
+else:
+    print ("no we did't get it")
+
