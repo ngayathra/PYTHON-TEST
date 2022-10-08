6028951adb0b6e0abd717200dc548156685963b4
gayathra <ngayathra@gmail.com>
2022-10-08 21:15:21 +0530

    dictionary checked

---
+++ Begining/dic.py
@@ -0,0 +1,36 @@
+from turtle import TPen
+
+
+mydic = {
+    "name" : "Gayathra",
+    "age" : "28",
+    "color" : "Blue"
+}
+
+# print (mydic)
+# print (type(mydic))
+
+#copying a dic
+
+# newdic = mydic.copy()
+# print (newdic)
+
+
+# another_dic = dict(mydic)
+# print (another_dic)
+
+# for x in mydic:
+    # print (mydic[x])
+
+# for x in mydic.values():
+#     print (x)
+
+# for y in mydic.keys():
+    # print (y)
+
+# for x, y in mydic.items():
+    # print (x, y)
+
+x = mydic.keys()
+print (x)
+print (type(x))
